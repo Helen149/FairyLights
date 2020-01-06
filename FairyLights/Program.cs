fd96e728521c6edd48c4ed31a7a88db9d7a347ad
Helen149 <Elena3565618@mail.ru>
2020-01-06 14:55:47 +0700

    Реализованы классы Lights и GameModel с отображением на форме

--- FairyLights/Program.cs
+++ FairyLights/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,17 +7,81 @@ using System.Windows.Forms;
 
 namespace FairyLights
 {
-    static class Program
+    class GameModel
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public static Lights[] lights;
+        public static void CreateLights(int count)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            lights = new Lights[count];
+            for(int i=0; i<count; i++)
+            {
+               lights[i] = new Lights(Tuple.Create(i*100, i*100), i%2==0);
+            }
+            
+        }
+
+        public static int GetXLight(int number)
+        {
+            
+            return lights[number].GetCoordinate().Item1;
+        }
+
+        public static int GetYLight(int number)
+        {
+
+            return lights[number].GetCoordinate().Item2;
+        }
+
+        public static bool GetStateLight(int number)
+        {
+            return lights[number].GetState();
+        }
+    }
+
+    class Lights 
+    {
+        Tuple<int, int> coordinate;
+        bool stateOn;
+
+        public Lights(Tuple<int, int> coordinate, bool stateOn)
+        {
+            this.coordinate = coordinate;
+            this.stateOn = stateOn;
+        }
+        public Tuple<int, int> GetCoordinate()
+        {
+            return coordinate;
+        }
+
+        public bool GetState()
+        {
+            return stateOn;
+        }
+    }
+
+    class MyForm : Form
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            for(int i=0; i<GameModel.lights.Length; i++)
+            {
+                Brush color = GameModel.GetStateLight(i) ? Brushes.Red : Brushes.Black;
+                graphics.FillEllipse(color, GameModel.GetXLight(i), GameModel.GetYLight(i), 100, 100);
+            }
+
+        }
+
+
+        public MyForm(GameModel game)
+        {
+            GameModel.CreateLights(3);
+        }
+
+        public static void Main()
+        {
+            var game = new GameModel();
+            Application.Run(new MyForm (game){ ClientSize = new Size(500, 500) });
         }
     }
 }
