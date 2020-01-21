cc121078169786df1ce3842762a9c86e437a403d
Helen149 <Elena3565618@mail.ru>
2020-01-21 17:30:48 +0700

    Добавлены тесты для метода FindEndX класса ParamInModel

--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -1,13 +1,19 @@
 using NUnit.Framework;
 using FairyLights;
+using System;
 
 namespace NUnitTestProject
 {
     [TestFixture]
-    public class Tests
+    public class FindEndX
     {
         [TestCase(0,-5)]
-        public void Test1(int direction, int ans)
+        [TestCase(1, 5)]
+        [TestCase(2, 10)]
+        [TestCase(3, 5)]
+        [TestCase(4, -5)]
+        [TestCase(5, -10)]
+        public void PositiveTest(int direction, int ans)
         {
             var param = new ParamInModel(20,20,0);
             var wire = new Wires(new Point(0, 0), direction, param.Length);
@@ -15,6 +21,16 @@ namespace NUnitTestProject
             var result = param.FindEndX(wire);
 
             Assert.AreEqual(ans, result);
+        }
+
+        [TestCase(6)]
+        [TestCase(-1)]
+        public void NegativeTest(int direction)
+        {
+            var param = new ParamInModel(20, 20, 0);
+            var wire = new Wires(new Point(0, 0), direction, param.Length);
+
+            Assert.Catch<Exception>(() =>  param.FindEndX(wire));
         }
     }
 }
