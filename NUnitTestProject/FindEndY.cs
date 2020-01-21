11d1d8d68c18e57c4cd92d4192d4eea9dfc6a887
Helen149 <Elena3565618@mail.ru>
2020-01-21 17:35:07 +0700

    Добавлены тесты метода FindEndY класса ParamInModel

---
+++ NUnitTestProject/FindEndY.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using FairyLights;
+using System;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class FindEndY
+    {
+        [TestCase(0, -10)]
+        [TestCase(1, -10)]
+        [TestCase(2, 0)]
+        [TestCase(3, 10)]
+        [TestCase(4, 10)]
+        [TestCase(5, 0)]
+        public void PositiveTest(int direction, int ans)
+        {
+            var param = new ParamInModel(20, 20, 0);
+            var wire = new Wires(new Point(0, 0), direction, param.Length);
+
+            var result = param.FindEndY(wire);
+
+            Assert.AreEqual(ans, result);
+        }
+
+        [TestCase(6)]
+        [TestCase(-1)]
+        public void NegativeTest(int direction)
+        {
+            var param = new ParamInModel(20, 20, 0);
+            var wire = new Wires(new Point(0, 0), direction, param.Length);
+
+            Assert.Catch<Exception>(() => param.FindEndY(wire));
+        }
+    }
+}
