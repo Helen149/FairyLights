9479b18f7e5f5d6fe3c5ce9599af104c732deb90
Helen149 <Elena3565618@mail.ru>
2020-01-22 16:22:56 +0700

    Добавлен тест для конструктора класса Lights

---
+++ NUnitTestProject/Consturctors/Lights.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using FairyLights;
+using System;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class TestLights
+    {
+
+        [Test]
+        public void CreateTest()
+        {
+            Random rnd = new Random();
+            var point = new Point(rnd.Next(), rnd.Next());
+            bool state = rnd.Next()%2==0?true:false;
+            int radius = rnd.Next();
+
+            var lights = new Lights(point, state, radius);
+
+            Assert.AreEqual(point, lights.Coordinate);
+            Assert.AreEqual(state, lights.StateOn);
+            Assert.AreEqual(radius, lights.Radius);
+        }
+    }
+}
