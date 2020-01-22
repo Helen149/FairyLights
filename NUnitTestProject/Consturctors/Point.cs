47e64c673f47a6cb07020bb7b28e4ab73c835b67
Helen149 <Elena3565618@mail.ru>
2020-01-22 16:07:06 +0700

    Добавлен тест для конструктора класса Point

---
+++ NUnitTestProject/Consturctors/Point.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using FairyLights;
+using System;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class TestPoint
+    {
+
+        [Test]
+        public void CreateTest()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next();
+            int y = rnd.Next();
+
+            var point = new Point(x, y);
+
+            Assert.AreEqual(x, point.X);
+            Assert.AreEqual(y, point.Y);
+        }
+    }
+}
