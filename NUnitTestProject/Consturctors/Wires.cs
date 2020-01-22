a289f8b88feff10242a7466a0c40dcc0eb416c72
Helen149 <Elena3565618@mail.ru>
2020-01-22 16:15:25 +0700

    Добавлен тест для конструктора класса Wires

--- NUnitTestProject/Consturctors/Wires.cs
+++ NUnitTestProject/Consturctors/Wires.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using FairyLights;
+using System;
 
-namespace NUnitTestProject.Consturctors
+namespace NUnitTestProject
 {
-    class Wires
+    [TestFixture]
+    public class TestWires
     {
+
+        [Test]
+        public void CreateTest()
+        {
+            Random rnd = new Random();
+            var point = new Point(rnd.Next(), rnd.Next());
+            int direction = rnd.Next();
+            int length = rnd.Next();
+
+            var wires = new Wires(point, direction,length);
+
+            Assert.AreEqual(point, wires.CoordinateStart);
+            Assert.AreEqual(direction, wires.Direction);
+            Assert.AreEqual(length, wires.Length);
+        }
     }
 }
+
