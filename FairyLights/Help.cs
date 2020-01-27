4e6dcf74cc3650efe84164e27b9446ddee3299e4
Helen149 <Elena3565618@mail.ru>
2020-01-27 09:54:28 +0700

    Добавлен класс справки Help

---
+++ FairyLights/Help.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyLights
+{
+    public class Help
+    {
+        public string Rules { get; private set; }
+
+        public Help()
+        {
+            Rules = "Цель: собрать схему так, чтобы все лампочки загорелись.\n\n" +
+                    "Управление осуществляется мышкой:\n" +
+                    "Левый клик - поворот по часовой стрелке\n" +
+                    "Правый клик - поворот против часовой стрелки\n\n" +
+                    "Удачи!";
+        }
+    }
+}
